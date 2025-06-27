1794d8ef397a3094a5d9d7d2ff37b81f24a54126
cruzmenezes <cruz.menezes@oulook.com>
2025-06-26 21:41:47 -0300

    lesson 2

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -7,4 +7,14 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
 
   })
+  it.only('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('Edson Victor')
+    cy.get('#lastName').type('menezes')
+    cy.get('#email').type('cruz.menezes@outlook.com')
+    cy.get('#phone').type('11 96182-1385')
+    cy.get('#open-text-area').type('Jesus é o caminho a verdade e a vida!!!!')
+    cy.get('.button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+  })
 })
