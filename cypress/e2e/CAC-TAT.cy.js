b21d324c7df233ea80d26f1cfbbd87cf35e3231e
cruzmenezes <cruz.menezes@outlook.com>
2025-07-03 16:01:17 -0300

    exercicio extra 2

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -8,13 +8,27 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
   })
   it.only('preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = Cypress._.repeat('Jesus é o caminho a verdade e a vida!!!!', 10)
     cy.get('#firstName').type('Edson Victor')
     cy.get('#lastName').type('menezes')
     cy.get('#email').type('cruz.menezes@outlook.com')
     cy.get('#phone').type('11 96182-1385')
-    cy.get('#open-text-area').type('Jesus é o caminho a verdade e a vida!!!!')
+    cy.get('#open-text-area').type(longText, {delay: 0})
     cy.get('.button[type="submit"]').click()
 
     cy.get('.success').should('be.visible')
+  })
+
+  it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    y.get('#firstName').type('Edson Victor')
+        cy.get('#lastName').type('menezes')
+        cy.get('#email').type('cruz.menezes@outlook,com')
+        cy.get('#phone').type('11 96182-1385')
+        cy.get('#open-text-area').type('teste')
+        cy.get('.button[type="submit"]').click()
+
+        cy.get('.erro').should('be.visible')
+
+
   })
 })
